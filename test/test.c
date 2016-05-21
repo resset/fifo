1680df9af7070b73ba760f66f53c74057640e68f
Mateusz Tomaszkiewicz <silentdemon@gmail.com>
2016-05-22 01:23:42 +0200

    test: filled setup function, more fifo_init tests

--- test/test.c
+++ test/test.c
@@ -4,8 +4,15 @@
 #include "unity.h"
 #include "test.h"
 
+fifo_t fifo;
+const uint16_t SIZE = UINT16_MAX;
+uint8_t buffer[UINT16_MAX];
+
 void setUp(void)
 {
+    if (FIFO_SUCCESS != fifo_init(&fifo, buffer, SIZE)) {
+        TEST_FAIL_MESSAGE("Cannot init fifo.");
+    }
 }
 
 void tearDown(void)
@@ -14,29 +21,33 @@ void tearDown(void)
 
 void test_fifo_init(void)
 {
-    fifo_t fifo;
-    const uint16_t SIZE = UINT16_MAX;
-    uint8_t buffer[SIZE];
-
+    /* Initialization errors. */
     TEST_ASSERT_TRUE(FIFO_ERROR == fifo_init(NULL, buffer, SIZE));
     TEST_ASSERT_TRUE(FIFO_ERROR == fifo_init(&fifo, NULL, SIZE));
     TEST_ASSERT_TRUE(FIFO_ERROR == fifo_init(&fifo, buffer, 0));
     TEST_ASSERT_TRUE(FIFO_ERROR == fifo_init(NULL, NULL, 0));
 
-    /* Random errors. */
+    /* Random initialization errors. */
     TEST_ASSERT_TRUE(FIFO_ERROR == fifo_init(&fifo, 0, SIZE));
     TEST_ASSERT_TRUE(FIFO_ERROR == fifo_init(0, buffer, SIZE));
 
-    /* -1 will be casted to uint16_t and will result in 65535
-     * which is perfectly correct value.
-     */
-    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_init(&fifo, buffer, -1));
-
+    /* Now some positive tests. */
     TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_init(&fifo, buffer, SIZE));
     TEST_ASSERT_NOT_NULL(fifo.buffer);
     TEST_ASSERT_EQUAL_UINT16(SIZE, fifo.size);
     TEST_ASSERT_EQUAL_UINT16(0, fifo.first);
     TEST_ASSERT_EQUAL_UINT16(0, fifo.elements_n);
+
+    /* Multiple initialization is not an error. */
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_init(&fifo, buffer, SIZE));
+
+    /* -1 will be casted to uint16_t and will result in 65535
+     * which is correct value.
+     */
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_init(&fifo, buffer, -1));
+
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_init(&fifo, buffer, UINT16_MAX));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_init(&fifo, buffer, INT8_MIN));
 }
 
 int main(void)
