8a4900a98f08bfa20ab37217831d8ace3770c9cd
Mateusz Tomaszkiewicz <silentdemon@gmail.com>
2016-05-26 15:24:46 +0200

    test: added some more tests (push mainly)

---
+++ test/test_fifo_operations.c
@@ -0,0 +1,128 @@
+#include <inttypes.h>
+
+#include "fifo.h"
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP(fifo_operations);
+
+static fifo_t fifo;
+static const uint16_t SIZE = UINT16_MAX;
+static uint8_t buffer[UINT16_MAX];
+
+TEST_SETUP(fifo_operations)
+{
+    if (FIFO_ERROR == fifo_init(&fifo, buffer, SIZE)) {
+        TEST_FAIL_MESSAGE("Error while initializing FIFO.");
+    }
+}
+
+TEST_TEAR_DOWN(fifo_operations)
+{
+}
+
+TEST(fifo_operations, test_push_one_byte)
+{
+    fifo_t fifo;
+    const uint16_t SIZE = 5;
+    uint8_t buffer[SIZE];
+
+    fifo_init(&fifo, buffer, SIZE);
+
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x5A));
+
+    TEST_ASSERT_EQUAL_HEX8(0x5A, fifo.buffer[0]);
+}
+
+TEST(fifo_operations, test_push_fill_buffer)
+{
+    fifo_t fifo;
+    const uint16_t SIZE = 5;
+    uint8_t buffer[SIZE];
+
+    fifo_init(&fifo, buffer, SIZE);
+
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x5A));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x4B));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x3C));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x2D));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x1E));
+
+    TEST_ASSERT_EQUAL_HEX8(0x5A, fifo.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX8(0x4B, fifo.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX8(0x3C, fifo.buffer[2]);
+    TEST_ASSERT_EQUAL_HEX8(0x2D, fifo.buffer[3]);
+    TEST_ASSERT_EQUAL_HEX8(0x1E, fifo.buffer[4]);
+}
+
+TEST(fifo_operations, test_push_overfill_buffer)
+{
+    fifo_t fifo;
+    const uint16_t SIZE = 5;
+    uint8_t buffer[SIZE];
+
+    fifo_init(&fifo, buffer, SIZE);
+
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x5A));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x4B));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x3C));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x2D));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x1E));
+
+    /* This push should fail. */
+    TEST_ASSERT_TRUE(FIFO_ERROR == fifo_push(&fifo, 0x0F));
+
+    TEST_ASSERT_EQUAL_HEX8(0x5A, fifo.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX8(0x4B, fifo.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX8(0x3C, fifo.buffer[2]);
+    TEST_ASSERT_EQUAL_HEX8(0x2D, fifo.buffer[3]);
+    TEST_ASSERT_EQUAL_HEX8(0x1E, fifo.buffer[4]);
+}
+
+TEST(fifo_operations, test_push_overfill_large_buffer)
+{
+
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x5A));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x4B));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x3C));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x2D));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x1E));
+
+    /* This push should fail. */
+    TEST_ASSERT_TRUE(FIFO_ERROR == fifo_push(&fifo, 0x0F));
+
+    TEST_ASSERT_EQUAL_HEX8(0x5A, fifo.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX8(0x4B, fifo.buffer[1]);
+    TEST_ASSERT_EQUAL_HEX8(0x3C, fifo.buffer[2]);
+    TEST_ASSERT_EQUAL_HEX8(0x2D, fifo.buffer[3]);
+    TEST_ASSERT_EQUAL_HEX8(0x1E, fifo.buffer[4]);
+}
+
+TEST(fifo_operations, test_ef)
+{
+    fifo_t ef_fifo;
+    const uint16_t EF_SIZE = UINT16_MAX;
+    uint8_t ef_buffer[EF_SIZE];
+
+    fifo_init(&ef_fifo, ef_buffer, EF_SIZE);
+
+    TEST_ASSERT_TRUE(FIFO_TRUE == fifo_is_empty(&ef_fifo));
+    TEST_ASSERT_TRUE(FIFO_FALSE == fifo_is_full(&ef_fifo));
+
+    fifo_push(&ef_fifo, 0);
+
+    TEST_ASSERT_TRUE(FIFO_FALSE == fifo_is_empty(&ef_fifo));
+    TEST_ASSERT_TRUE(FIFO_FALSE == fifo_is_full(&ef_fifo));
+
+    for (uint16_t i = 1; i < EF_SIZE - 1; i++) {
+        fifo_push(&ef_fifo, i);
+    }
+
+    TEST_ASSERT_TRUE(FIFO_FALSE == fifo_is_empty(&ef_fifo));
+    TEST_ASSERT_TRUE(FIFO_FALSE == fifo_is_full(&ef_fifo));
+
+    fifo_push(&ef_fifo, EF_SIZE - 1);
+
+    TEST_ASSERT_TRUE(FIFO_FALSE == fifo_is_empty(&ef_fifo));
+    TEST_ASSERT_TRUE(FIFO_TRUE == fifo_is_full(&ef_fifo));
+}
--- test/test_runners/all_tests.c
+++ test/test_runners/all_tests.c
@@ -3,6 +3,7 @@
 static void run_all_tests(void)
 {
     RUN_TEST_GROUP(fifo_initializations);
+    RUN_TEST_GROUP(fifo_operations);
 }
 
 int main(int argc, const char *argv[])
