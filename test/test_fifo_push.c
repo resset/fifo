1cc04d834de0a052e1ddda26702ceda47fabf836
Mateusz Tomaszkiewicz <silentdemon@gmail.com>
2016-06-04 00:03:46 +0200

    test: added new push test, simplified code
    Brought back common initialization code

--- test/test_fifo_push.c
+++ test/test_fifo_push.c
@@ -6,8 +6,15 @@
 
 TEST_GROUP(test_fifo_push);
 
+static fifo_t fifo;
+static const uint16_t SIZE = 5;
+static uint8_t buffer[5];
+
 TEST_SETUP(test_fifo_push)
 {
+    if (FIFO_ERROR == fifo_init(&fifo, buffer, SIZE)) {
+        TEST_FAIL_MESSAGE("Error while initializing FIFO.");
+    }
 }
 
 TEST_TEAR_DOWN(test_fifo_push)
@@ -16,12 +23,6 @@ TEST_TEAR_DOWN(test_fifo_push)
 
 TEST(test_fifo_push, test_push_one_byte)
 {
-    fifo_t fifo;
-    const uint16_t SIZE = 5;
-    uint8_t buffer[SIZE];
-
-    fifo_init(&fifo, buffer, SIZE);
-
     TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x5A));
 
     TEST_ASSERT_EQUAL_HEX8(0x5A, fifo.buffer[0]);
@@ -29,12 +30,6 @@ TEST(test_fifo_push, test_push_one_byte)
 
 TEST(test_fifo_push, test_push_fill_buffer)
 {
-    fifo_t fifo;
-    const uint16_t SIZE = 5;
-    uint8_t buffer[SIZE];
-
-    fifo_init(&fifo, buffer, SIZE);
-
     TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x5A));
     TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x4B));
     TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x3C));
@@ -50,12 +45,6 @@ TEST(test_fifo_push, test_push_fill_buffer)
 
 TEST(test_fifo_push, test_push_overfill_buffer)
 {
-    fifo_t fifo;
-    const uint16_t SIZE = 5;
-    uint8_t buffer[SIZE];
-
-    fifo_init(&fifo, buffer, SIZE);
-
     TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x5A));
     TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x4B));
     TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x3C));
@@ -83,15 +72,46 @@ TEST(test_fifo_push, test_push_overfill_large_buffer)
     }
 
     for (uint16_t i = 0; i < UINT16_MAX; i++) {
-        TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, (uint8_t)(i & 0xFF)));
+        TEST_ASSERT_TRUE(FIFO_SUCCESS ==
+                         fifo_push(&fifo, (uint8_t) (i & 0xFF)));
     }
+
+    /* These pushes should fail. */
+    TEST_ASSERT_TRUE(FIFO_ERROR == fifo_push(&fifo, 0x0F));
+    TEST_ASSERT_TRUE(FIFO_ERROR == fifo_push(&fifo, 0x1F));
+    TEST_ASSERT_TRUE(FIFO_ERROR == fifo_push(&fifo, 0x2F));
+
+    for (uint16_t i = 0; i < UINT16_MAX; i++) {
+        TEST_ASSERT_EQUAL_HEX8((uint8_t) (i & 0xFF), fifo.buffer[i]);
+    }
+}
+
+TEST(test_fifo_push, test_push_fill_buffer_not_from_zero_index)
+{
+    /* First some dummy pushes. */
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x5A));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x4B));
+
+    /* And now we pull data back from queue. */
+    fifo_pop(&fifo);
+    fifo_pop(&fifo);
+
+    /* We still should have 5 empty places albeit not starting from index 0. */
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x5A));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x4B));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x3C));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x2D));
+    TEST_ASSERT_TRUE(FIFO_SUCCESS == fifo_push(&fifo, 0x1E));
 
     /* This push should fail. */
     TEST_ASSERT_TRUE(FIFO_ERROR == fifo_push(&fifo, 0x0F));
 
-    for (uint16_t i = 0; i < UINT16_MAX; i++) {
-        TEST_ASSERT_EQUAL_HEX8((uint8_t)(i & 0xFF), fifo.buffer[i]);
-    }
+    /* Now we start from third element. */
+    TEST_ASSERT_EQUAL_HEX8(0x5A, fifo.buffer[2]);
+    TEST_ASSERT_EQUAL_HEX8(0x4B, fifo.buffer[3]);
+    TEST_ASSERT_EQUAL_HEX8(0x3C, fifo.buffer[4]);
+    TEST_ASSERT_EQUAL_HEX8(0x2D, fifo.buffer[0]);
+    TEST_ASSERT_EQUAL_HEX8(0x1E, fifo.buffer[1]);
 }
 
 TEST(test_fifo_push, test_ef)
--- test/test_runners/test_fifo_push_runner.c
+++ test/test_runners/test_fifo_push_runner.c
@@ -6,5 +6,6 @@ TEST_GROUP_RUNNER(test_fifo_push)
     RUN_TEST_CASE(test_fifo_push, test_push_one_byte);
     RUN_TEST_CASE(test_fifo_push, test_push_fill_buffer);
     RUN_TEST_CASE(test_fifo_push, test_push_overfill_buffer);
-//     RUN_TEST_CASE(test_fifo_push, test_push_overfill_large_buffer);
+    RUN_TEST_CASE(test_fifo_push, test_push_overfill_large_buffer);
+    RUN_TEST_CASE(test_fifo_push, test_push_fill_buffer_not_from_zero_index);
 }
